405a953ad9855f59ae3879b4987e879342cc7d35
celand42 <celand42@students.tntech.edu>
2017-02-22 21:23:30 -0600

    long time since I commited, lots of things done

---
+++ app/src/main/java/com/example/chaseland/moviepostermvp/data/source/remote/PosterRemoteDataSource.java
@@ -0,0 +1,8 @@
+package com.example.chaseland.moviepostermvp.data.source.remote;
+
+/**
+ * Created by chaseland on 1/1/17.
+ */
+
+public class PosterRemoteDataSource {
+}
---
+++ app/src/main/java/com/example/chaseland/moviepostermvp/posters/PostersPresenter.java
@@ -0,0 +1,8 @@
+package com.example.chaseland.moviepostermvp.posters;
+
+/**
+ * Created by chaseland on 1/11/17.
+ */
+
+public class PostersPresenter implements Presenter {
+}
